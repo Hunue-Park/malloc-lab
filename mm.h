390df9f8e8d7ea811b311791e1cd119b33795be1
Ubuntu <ubuntu@ip-172-31-37-77.ap-northeast-2.compute.internal>
2021-12-10 14:09:50 +0000

    write code at text book

--- mm.h
+++ mm.h
@@ -2,8 +2,10 @@
 
 extern int mm_init (void);
 extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
+extern void mm_free (void *bp);
 extern void *mm_realloc(void *ptr, size_t size);
+static void *coalesce(void *bp);
+static void *extend_heap(size_t words);
 
 
 /* 
